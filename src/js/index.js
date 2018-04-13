74eab982a266ef34dcf9ee55e202e89dfa201ba9
chihuomeizi <1596396737@qq.com>
2018-04-13 19:41:33 +0800

    菜单列表功完成

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,71 @@
 $(function(){
-  var mySwiper = new Swiper('.swiper-container', {
-  autoplay: true,//可选选项，自动滑动
-})
+
+//菜单处理
+function loadMenuData(){
+  return axios.get('home/catitems');
+}
+
+function renderMenu(paramData){
+   return  new Promise(function(resolve,reject){
+      let text = template('menuTpl',{list:paramData.data});
+      $("#menuInfo").html(text);
+      resolve();
+   })
+}
+ 
+
+ //轮播图
+ //获取轮播图的数据
+ function loadSwiperData(){
+  return axios.get('home/swiperdata');
+ }
+ // 渲染轮播图模板
+ function renderSwiper(paramData){
+   return new Promise(function(resolve,reject){
+      let html = template('swiperTpl',{list:paramData.data});
+      $('#swiperinfo').html(html);
+      resolve();
+   });
+
+ }
+ function handleSwiper(){
+  return new Promise(function(resolve,reject){
+        new Swiper('.swiper-container',{
+        loop : true,
+        pagination: {
+        el: '.swiper-pagination',
+       },
+       autoplay: {
+        disableOnInteraction: false,
+      }
+    })
+        resolve();
+  })
+ }
+
+   //页面初始化完成之后,触发改事件
+  $(document).on("pageInit", function(e, pageId, $page) {
+    // 处理轮播效果
+    loadSwiperData()
+      .then(renderSwiper)
+      .then(handleSwiper)
+      .then(function(){
+        $.toast('success');
+      })
+      .catch(function(){
+        $.toast('服务器错误');
+      })
+
+      //菜单
+      loadMenuData()
+      .then(renderMenu)
+      .then(function(){
+        $.toast("success");
+      })
+     .catch(function(){
+      $.toast("响应失败")
+     })
+  });
+  $.init();
+
 })
